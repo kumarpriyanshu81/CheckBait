4986d2f7ab3b2df4fe0b6aa8fe7ba6550c5e1e91
kumarpriyanshu81 <kumarpriyanshu81@gmail.com>
2023-04-29 00:21:34 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+import pickle
+from ssl_checker import SSLChecker
+
+app = Flask(__name__)
+ssl_checker = SSLChecker()
+
+# Load the ML model and the vectorizer
+filename = "logistic.pickle"
+loaded_model = pickle.load(open(filename, "rb"))
+loaded_vectorizer = pickle.load(open('/home/luc1fer/Documents/Minor_Project/phishingchecker/vectorizer.pickle',"rb"))
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the data from the POST request
+    url = request.args['url']
+    # Transform the input data using the vectorizer
+    test_x_d = loaded_vectorizer.transform([url])
+    # Use the loaded ML model to make predictions
+    y_pred = loaded_model.predict(test_x_d)
+    # Return the prediction result as a JSON response
+    return jsonify({'prediction': y_pred[0]})
+
+@app.route('/check_ssl', methods=['POST'])
+def check_ssl():
+    hosts = request.args.getlist('hosts')
+    if not hosts:
+        return {'error': 'Please provide a list of hosts to check.'}, 400
+    results = {}
+    for host in hosts:
+        try:
+            host, port = ssl_checker.filter_hostname(host)
+            cert = ssl_checker.get_cert(host, port)
+            cert_info = ssl_checker.get_cert_info(host, cert)
+            cert_info['tcp_port'] = int(port)
+            results[host] = cert_info
+        except Exception as e:
+            results[host] = {'error': str(e)}
+    return results, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
